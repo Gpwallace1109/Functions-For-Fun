62e4af22ec940603a1d3b9e93e97b551be04b7e7
Gpwallace1109 <147451469+Gpwallace1109@users.noreply.github.com>
2023-11-05 11:07:03 -0600

    My Name is Lewis.js

---
+++ My Name is Lewis.js
@@ -0,0 +1,26 @@
+// Functions For Fun...Expressions vs Declarations
+
+// Expression
+var question = function () { 
+    console.log('Whoa...a Jackolantern!');
+};
+
+// Expression
+var jackolantern = function () { 
+    console.log('I am not a Jackolantern...');
+};
+
+// Declaration
+var lewis = () => { 
+    console.log('My name....is Leeewis...');
+}; 
+
+// Declaration
+var dang = () => {
+    console.log('Did that thing just talk?....');
+};
+
+question();
+jackolantern(); 
+lewis(); 
+dang();
